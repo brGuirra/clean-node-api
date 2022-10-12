71683937123b5349357263bed4f33b89664cecaf
brGuirra <br.guirra@gmail.com>
2022-10-12 18:19:55 -0300

    fix: adjust tsconfig to resolve non absolute imports

--- jest.config.ts
+++ jest.config.ts
@@ -1,14 +1,9 @@
 import type { JestConfigWithTsJest } from 'ts-jest';
-import { compilerOptions } from './tsconfig.json';
-import { pathsToModuleNameMapper } from 'ts-jest';
 
 const jestConfig: JestConfigWithTsJest = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  roots: ['<rootDir>/src'],
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
-  coverageDirectory: 'coverage',
-  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>/' }),
+  moduleDirectories: ['node_modules', 'src'],
 };
 
 export default jestConfig;
